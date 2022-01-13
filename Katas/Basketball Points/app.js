865d53f5146468c1fd68d8b7557451dd4148c84e
sebastianborjesson <sebastian.borjesson@live.se>
2022-01-13 08:30:34 +0100

    added comments

--- Katas/Basketball Points/app.js
+++ Katas/Basketball Points/app.js
@@ -1,6 +1,8 @@
 function points(twoPointers, threePointers) {
+    // Each argument multiply with it's value
     twoPointers *= 2;
     threePointers *= 3;
+    // Add the values together 
     const sum = twoPointers + threePointers;
     console.log(sum);
 }
