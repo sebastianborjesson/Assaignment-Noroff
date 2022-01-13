6037d1b9d26d79effc0f0cb812e349bff29612b4
sebastianborjesson <sebastian.borjesson@live.se>
2022-01-13 08:27:57 +0100

    add new folders + basketball kata

---
+++ Katas/Basketball Points/app.js
@@ -0,0 +1,13 @@
+function points(twoPointers, threePointers) {
+    twoPointers *= 2;
+    threePointers *= 3;
+    const sum = twoPointers + threePointers;
+    console.log(sum);
+}
+
+points(1, 1);
+points(7, 5);
+points(38, 8);
+points(0, 1);
+points(0, 0);
+
